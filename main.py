c72a4280e88b808ead7720e6afc54343569461c3
JOSHUA EFRAIN CUEVA CAMPOVERDE joshua.cueva@ucuenca.edu.ec <joshua.cueva@ucuenca.edu.ec>
2024-10-06 21:31:00 -0500

    Created proyect structure

---
+++ main.py
@@ -0,0 +1,8 @@
+
+"""
+El proyecto está basado en mvc (que es lo más fácil), pero separado en módulos
+con vertical slicing. Cada slice es un módulo que contiene los tres componentes de mvc.
+"""
+
+if __name__ == '__main__':
+    pass
