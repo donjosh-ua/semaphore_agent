5d3201d13cf5d60e6d0d6c6df57264c973512f6b
JOSHUA EFRAIN CUEVA CAMPOVERDE joshua.cueva@ucuenca.edu.ec <joshua.cueva@ucuenca.edu.ec>
2024-10-08 18:07:54 -0500

    Changed some classes's names

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from intersection.controller.controller import Controlador
+from src.intersection.controller.Controller import Controller
+
 """
 El proyecto está basado en mvc (que es lo más fácil), pero separado en módulos
 con vertical slicing. Cada slice es un módulo que contiene los tres componentes de mvc.
 """
 
 if __name__ == '__main__':
-    #view = Ventana()
-    ctrl = Controlador()
+    Controller()
--- src/intersection/controller/ctrl.py
+++
@@ -1,9 +0,0 @@
-from ..view.view import Ventana
-
-class Controlador:
-    view = Ventana()
-
-    """Se debe pasar que imagen se desea que se mueva, una velocidad horizontal y una velocidad vertical """
-    view.mover_imagen(view.semaforo_inferior, 5, 5)
-
-    view.ventana.mainloop()
